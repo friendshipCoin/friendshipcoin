fed60bbaa6e4fd9845a6042e3dd1fa84dff558b5
Ubuntu <ubuntu@ip-172-31-31-234.us-west-2.compute.internal>
2014-02-12 03:58:26 +0000

    update

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 46383 : 37637;
+    return testnet ? 46383 : 37636;
 }
 
 
